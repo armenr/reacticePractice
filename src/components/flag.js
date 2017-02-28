270164cad49762916351c6e75d0d4613dda24a79
Armen Rostamian <armenr@gmail.com>
2017-02-27 20:54:27 -0800

    more assets, starting out the flag

---
+++ src/components/flag.js
@@ -0,0 +1,32 @@
+'use strict';
+
+import React from 'react';
+
+const data = {
+  'cu': {
+    'name': 'Cuba',
+    'icon': 'flag-cu.png',
+  },
+  'fr': {
+    'name': 'France',
+    'icon': 'flag-fr.png',
+  },
+  'jp': {
+    'name': 'Japan',
+    'icon': 'flag-jp.png',
+  },
+  'nl': {
+    'name': 'Netherlands',
+    'icon': 'flag-nl.png',
+  },
+  'uz': {
+    'name': 'Uzbekistan',
+    'icon': 'flag-uz.png',
+  }
+};
+
+const flag = () => {
+  render() {
+
+  }
+}
